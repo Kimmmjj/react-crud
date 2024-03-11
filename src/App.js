cfc1f77681d2a2b54a114b8d6bed315c74fafaa1
JeongHwan Kim <silverice1973@naver.com>
2024-03-11 22:13:25 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [value, setValue] = useState('');
+  const [cost, setCost] = useState('');
+
+  function onSubmit() {
+    const cost = parseInt(document.querySelector('input[name="cost"]').value);
+
+
+    const item = {
+      title: value,
+      cost,
+    };
+
+    setItems([...items, item]);
+    setValue('');
+    setCost('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="budget">
+        <h1>예산 계산기</h1>
+        <div className="item">
+          <div>지출</div>
+          <input
+            type="text"
+            name="value"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            style={{ flex: "2", padding: "5px" }}
+            placeholder="예) 렌트비"
+          />
+        </div>
+        <div className="item">
+          <div>항목 비용</div>
+          <input
+            type="number"
+            name="cost"
+            style={{ flex: "3", padding: "5px" }}
+            placeholder="0"
+          />
+        </div>
+        <input
+          type="submit"
+          value="제출"
+          className="btn"
+          onClick={onSubmit}
+        />
+      </div>
+      <div className="list">
+        {items.map((item) => (
+          <List key={item.title} title={item.title} cost={item.cost} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function List({ title, cost }) {
+  return (
+    <div className='list-container'>
+      <div className=''>
+        <div className="list">
+          <div className="title">{title}</div>
+          <div className="cost">{cost === undefined ? '0' : cost}원</div>
+          
+        </div>
+      </div>
     </div>
   );
 }
